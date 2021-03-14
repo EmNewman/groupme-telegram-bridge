35cba9a8386cd6f55281fcb0ed5467fd46d1d8de
Emily Newman <empatnewman@gmail.com>
2021-03-14 14:14:41 -0700

    Update to working version (no SSL)

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 import logging
-import secrets
+# import secrets
 import requests
-from telegram import Bot
+from telegram import Bot, Update
 from telegram.ext import MessageHandler, TypeHandler, Filters, Dispatcher
+# for heroku only
+import os
+import json
 # from groupy import Client
+
+TG_BOT_USERNAME = os.environ.get('TG_BOT_USERNAME')
+GROUPME_BOT_ID = os.environ.get('GROUPME_BOT_ID')
+TG_CHAT_ID = os.environ.get('TG_CHAT_ID')
+TG_BOT_API_TOKEN = os.environ.get('TG_BOT_API_TOKEN')
+WEBHOOK_URL = os.environ.get('WEBHOOK_URL')
+GROUPME_BOT_NAME = os.environ.get('GROUPME_BOT_NAME')
 
 # let's use flask
 from flask import Flask, request 
@@ -12,6 +22,41 @@ app = Flask(__name__)
 
 # set up groupme
 # client = Client.from_token(secrets.GROUPME_API_TOKEN)
+
+dispatcher = None
+bot = None
+
+
+@app.before_first_request
+def setup():
+    global bot, dispatcher
+    # set up logging
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                     level=logging.INFO)
+
+    # # TODO until i figure out webhooks we will just use polling.
+    # updater = Updater(token=TG_BOT_API_TOKEN, use_context=True)
+    # dispatcher = updater.dispatcher
+    # tg_text_handler = MessageHandler(Filters.text & (~Filters.command), tg_msg_handler)
+    # dispatcher.add_handler(tg_text_handler)
+
+    # Set up webhook for TG
+    bot = Bot(TG_BOT_API_TOKEN)
+    bot.set_webhook(WEBHOOK_URL+"/telegram")
+    dispatcher = Dispatcher(bot, None, workers=0)
+
+
+    # TODO add photo handling, see here: 
+    # https://python-telegram-bot.readthedocs.io/en/latest/telegram.message.html#telegram.Message 
+
+    # Add text handling
+    tg_text_handler = MessageHandler(Filters.text & (~Filters.command), tg_msg_handler)
+    dispatcher.add_handler(tg_text_handler)
+
+    # custom handler for receiving Groupme messages
+    groupme_handler = TypeHandler(GroupmeMessage, groupme_msg_handler)
+    dispatcher.add_handler(groupme_handler)
+
 
 class GroupmeMessage():
     def __init__(self, msg):
@@ -40,6 +85,7 @@ def webhook_groupme():
     # add message to dispatch
     gm_msg = GroupmeMessage(data)
     dispatcher.process_update(gm_msg)
+    return "Done"
 
 
 @app.route('/telegram', methods=['POST'])
@@ -47,64 +93,46 @@ def webhook_tg():
     data = request.get_json()
     print('Received {} to TG webhook'.format(data))
 
-    Update.de_json(json.loads(data), bot)
+    update = Update.de_json(data, bot)
     dispatcher.process_update(update)
-
+    return "Done"
 
 
 # yoink send_to_groupme from tech server
 def send_to_groupme(username, msg_text):
-    if username != secrets.TG_BOT_USERNAME:
-        requests.post("https://api.groupme.com/v3/bots/post", 
-                      data={'bot_id': secrets.GROUPME_BOT_ID,
-                            'text': username + ": " + msg_text
-        })
+    print("sending to groupme")
+    if username != TG_BOT_USERNAME:
+        data = {
+            'bot_id': GROUPME_BOT_ID,
+            'text': username.first_name + " " + username.last_name + ": " + msg_text
+        }
+        to_str = json.dumps(data)
+        result = requests.post("https://api.groupme.com/v3/bots/post", 
+                      data=to_str)
+        print(result.text)
 
 # Message handler for Telegram messages
 def tg_msg_handler(update, context):
     # receive message from TG
     message_content = update.message.text
     username = update.message.from_user
+    print("Got message from TG!")
+    print(update, context)
     # send to groupme
-    send_to_groupme(username, msg_text)
+    send_to_groupme(username, message_content)
 
 # Message handler for Groupme messages
 def groupme_msg_handler(update, context):
     # create message
+    print("Got message from Groupme!")
     msg = update.name + ": " + update.text 
-    if update.user_id == secrets.GROUPME_BOT_ID:
+    if update.name == GROUPME_BOT_NAME:
         return
     # send to TG
-    context.bot.send_message(chat_id=secrets.TG_CHAT_ID, text=msg)
-
-
-
-def setup_tg(token):
-    bot = Bot(token)
-    dispatcher = Dispatcher(bot, None, workers=0)
-    return dispatcher
-
-# set up logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                 level=logging.INFO)
-# setup tg
-dispatcher = setup_tg(secrets.TG_BOT_API_TOKEN)
-# TODO add photo handling, see here: 
-# https://python-telegram-bot.readthedocs.io/en/latest/telegram.message.html#telegram.Message 
-tg_text_handler = MessageHandler(Filters.text & (~Filters.command), tg_msg_handler)
-dispatcher.add_handler(tg_text_handler)
-
-# custom handler for receiving Groupme messages
-groupme_handler = TypeHandler(GroupmeMessage, groupme_msg_handler)
-dispatcher.add_handler(groupme_handler)
-
-
-
-
+    context.bot.send_message(chat_id=TG_CHAT_ID, text=msg)
 
 def main():
     app.run()
-
 
 if __name__ == '__main__':
 	main()
---
+++ env_vars.sh
@@ -0,0 +1,6 @@
+export TG_BOT_USERNAME="username"
+export GROUPME_BOT_ID="bot_id"
+export TG_CHAT_ID="chat_id"
+export TG_BOT_API_TOKEN="token"
+export WEBHOOK_URL="url"
+export GROUPME_BOT_NAME="bot_name"
